10173cd8a9eefc53adffabb22cba97202ab2acaa
Asmaul Husna <azh0268@auburn.edu>
2026-04-21 17:09:04 -0500

    Tried to run the iris_plot.py script
    Didn't run, first failure

---
+++ iris_plot.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python3
+
+"""
+Iris Plot Script
+
+This script reads the iris dataset, performs linear regression
+for each species, and generates scatter plots with regression lines.
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+def plot_species(dataframe, species):
+    """
+    Create regression plot for a given species
+    """
+    species_df = dataframe[dataframe.species == species]
+
+    x = species_df.petal_length_cm
+    y = species_df.sepal_length_cm
+
+    reg = stats.linregress(x, y)
+
+    plt.figure()
+    plt.scatter(x, y, label=f"{species} data")
+    plt.plot(x, reg.slope * x + reg.intercept, color="orange")
+
+    plt.xlabel("Petal length (cm)")
+    plt.ylabel("Sepal length (cm)")
+    plt.title(species)
+    plt.legend()
+
+    plt.savefig(f"{species}.png")
+    plt.close()
+
